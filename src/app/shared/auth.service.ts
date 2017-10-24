fd852fdcbe1ee4960f09c1c2325b96a829c8840a
selma <selma@gmail.com>
2017-10-24 12:48:50 +0200

    formulaire d'inscription

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,15 +1,14 @@
-import { Injectable, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { User } from "./user";
-import { Observable } from "rxjs/Observable";
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class AuthService {
-
-     /* Le BehaviorSubject est un type de Subject particulier
+  /* Le BehaviorSubject est un type de Subject particulier
    de RxJs qui a constamment une valeur, ce qui fait
    que lorsqu'on se subscribe dessus, on recevra la
    dernière valeur qui a été poussée dans le flux.
@@ -17,25 +16,23 @@ export class AuthService {
    au moment de son instanciation, on pourra ensuite
    lui pousser des valeurs avec la méthode .next(value)
   */
+  user:BehaviorSubject<User> = new BehaviorSubject(null);
+  urlAPI:string = 'http://localhost:3000/user';
 
-    user:BehaviorSubject<User> = new BehaviorSubject(null);
-    urlAPI:string = 'http://localhost:3001/user';
+  constructor(private http:HttpClient) { 
+    //Quand le service est créé, il va vérifier dans le localStorage si un user s'y trouve déjà, et si oui, il le récupère et le pousse dans le flux de user
+    this.user.next(JSON.parse(localStorage.getItem('user')));
+  }
+
+
+  login(username:string,pass:string):Observable<boolean> {
     
-    constructor(private http:HttpClient){
-  // Quand le service est créé, il va vérifier dans le localStorage si un user s'y trouve déjà, 
-  // et si oui, il le récupère et le pousse dans le flux de user
-        this.user.next(JSON.parse(localStorage.getItem('user')));
-
-    }
-
-    login(username:string, pass:string):Observable<boolean> {
-
     return this.http.get<User[]>(this.urlAPI+'?username='+username+'&password='+pass)
     .map((users) => {
       if(users.length === 1) {
-        /*On stock le user en localStorage (c'est pas 
-        très très bien de faire ça, en soit on voudrait
-        plutôt stocker juste une clef token en localStorage et pas tout le user*/
+        //On stock le user en localStorage (c'est pas 
+        //très très bien de faire ça, en soit on voudrait
+        //plutôt stocker juste une clef token en localStorage et pas tout le user)
         localStorage.setItem('user', JSON.stringify(users[0]));
         //On pousse le user dans le flux de User qu'est notre BehaviorSubject
         this.user.next(users[0]);
@@ -51,4 +48,11 @@ export class AuthService {
     this.user.next(null);
   }
 
+  signup(user:User):Observable<User> {
+    return this.http.post<User>(this.urlAPI, user);
+  }
+
+  getByUsername(username:String):Observable<User>{
+  return this.http.get<User>(this.urlAPI+'?username=' +username);
+  }
 }
---
+++ src/app/shared/validators.ts
@@ -0,0 +1,2 @@
+import { AsyncValidatorFn, AbstractControl, ValidationErrors } from "@angular/forms";
+import { Observable } from "rxjs/Observable";
